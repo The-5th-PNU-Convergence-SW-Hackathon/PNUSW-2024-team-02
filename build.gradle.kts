ae83e199ce0e2954383441801008f3e0af4eda7b
Karryun <165464282+Karryun@users.noreply.github.com>
2024-07-17 17:16:23 +0900

    Revert "Login_Update"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
